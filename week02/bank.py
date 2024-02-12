bec49bd7554ff8296bf8c1a0345c4b77ee0abe58
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-02-12 21:49:33 +0000

    $ python bank

---
+++ week02/bank.py
@@ -0,0 +1,10 @@
+# Author Gabriela Domiciano
+#$ python bank.py
+
+
+amount1 = int(input("Enter amount1(in cent): "))
+amount2 = int(input("Enter amount2(in cent): "))
+result = (amount1 + amount2) / 100
+print(f'The sun of these is €{result}')
+
+
