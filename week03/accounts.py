e2b43962a7a2462e7c129a13fd5e893406da2f84
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-02-15 21:38:44 +0000

    weeklytask

---
+++ week03/accounts.py
@@ -0,0 +1,12 @@
+# Weekly Task 03
+# Author Gabriela Domiciano
+
+
+# Account_number = 1234567890
+# First start with 0
+
+# Mask the first 6 digits with Xs and display the last 4 digits
+
+account_number_input = input("Enter the 10-character account number: ")
+last_four_number = account_number_input[-4]
+print('XXXXXX : ', last_four_number')
