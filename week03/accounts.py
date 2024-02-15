f04763665b87b418fd48ef4b28adec15c16fb44c
Gabriela Domiciano Avellar <gabriela.d@hotmail.com>
2024-02-15 22:06:51 +0000

    weeklytask03

--- week03/accounts.py
+++ week03/accounts.py
@@ -3,10 +3,23 @@
 
 
 # Account_number = 1234567890
-# First start with 0
+# First start with 0 and not with 1
 
 # Mask the first 6 digits with Xs and display the last 4 digits
 
+# sources: Youtube Chanel PromoAmbitions- Python String Tutorial
+
+
+
 account_number_input = input("Enter the 10-character account number: ")
-last_four_number = account_number_input[-4]
-print('XXXXXX : ', last_four_number')
+last_four_number = account_number_input[-4:]
+print ('XXXXXX{}' .format (last_four_number))
+
+
+
+# diferenbt length
+
+account_number_input = input("Enter the 5-character account number: ")
+last_two_number = account_number_input[-2:]
+print ('XXXXXX{}' .format (last_two_number))
+       
